b28c1feaa5477626be613f0a3e4810ad9770f74d
Ross McKinlay <pezi_pink@pinksquirrellabs.com>
2014-05-03 22:08:45 +0100

    add fighting fantasy project

---
+++ FightingFantasy/FightingFantasy.fs
@@ -0,0 +1,23 @@
+﻿namespace FightingFantasy
+
+type Dice() =
+    let rng = System.Random(System.DateTime.Now.Millisecond)
+    let d6() = rng.Next(1,7)
+
+type Skill = int
+type Luck = int
+type Stamina = int
+
+type Player =
+    { skill : Skill * Skill
+      Luck : Luck * Luck
+      Stamina : Stamina * Stamina }
+
+type GlobalState =
+    { player : Player
+      dice : Dice
+    }
+
+
+type Class1() = 
+    member this.X = "F#"
